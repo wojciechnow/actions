d5c247e57442aa62f1785ee0247d9d17c0fdd6f8
wojciechnowacki <wojciech.nowacki@softwarebrothers.co>
2020-02-12 09:45:19 +0100

    env check

--- test/randome.js
+++ test/randome.js
@@ -26,19 +26,34 @@ describe('my test', () => {
             .build();
     });
 
-
-    it('Just testing flow for selenium in SB company', async () => {
-        await driver.manage().window().maximize();
-        await driver.get('https://softwarebrothers.co/');
-        await driver.findElement(By.linkText("Services")).click();
-        await driver.sleep(2000);
-        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
-        await driver.sleep(2000);
-        await driver.findElement(By.className(("webdev"))).click();
-        await driver.sleep(2000);
-        const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-        expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-    });
+    const x = process.env.pusher;
+    if (x.includes('wojciech')) {
+        it('Just testing flow for selenium in SB company', async () => {
+            await driver.manage().window().maximize();
+            await driver.get('https://softwarebrothers.co/');
+            await driver.findElement(By.linkText("Services")).click();
+            await driver.sleep(2000);
+            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+            await driver.sleep(2000);
+            await driver.findElement(By.className(("webdev"))).click();
+            await driver.sleep(2000);
+            const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+            expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+        });
+    } else {
+        it('Pusher testing', async () => {
+            await driver.manage().window().maximize();
+            await driver.get('https://softwarebrothers.co/');
+            await driver.findElement(By.linkText("Services")).click();
+            await driver.sleep(2000);
+            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+            await driver.sleep(2000);
+            await driver.findElement(By.className(("webdev"))).click();
+            await driver.sleep(2000);
+            const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+            expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+        });
+    }
 
     after(async () => {
         await driver.quit()
