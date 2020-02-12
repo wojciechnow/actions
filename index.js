5d4de44b936be50a77fb3b4ad0df0815567f3743
wojciechnowacki <wojciech.nowacki@softwarebrothers.co>
2020-02-12 08:51:05 +0100

    run on gha

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
     const pushAuthor = github.context.payload.commits[0].author.email;
-    console.log(`The event payload: ${pushAuthor}`);
+    console.log(`User that pushed code: ${pushAuthor}`);
     core.setOutput("pusher");
 
 } catch (error) {
---
+++ test/randome.js
@@ -0,0 +1,46 @@
+// e2e/runWithSelenium.ts
+const webdriver = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+const assert = require("assert");
+const path = require("path");
+const {
+    Builder,
+    By,
+    Key,
+    until,
+    WebDriver,
+    Capabilities
+} = require('selenium-webdriver');
+const {
+    expect
+} = require('chai');
+
+let driver = webdriver.WebDriver;
+
+describe('my test', () => {
+    before(async () => {
+        require("chromedriver");
+        capabilities = webdriver.Capabilities.chrome();
+        driver = await new webdriver.Builder()
+            .withCapabilities(capabilities)
+            .build();
+    });
+
+
+    it('Just testing flow for selenium in SB company', async () => {
+        await driver.manage().window().maximize();
+        await driver.get('https://softwarebrothers.co/');
+        await driver.findElement(By.linkText("Services")).click();
+        await driver.sleep(2000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+        await driver.sleep(2000);
+        await driver.findElement(By.className(("webdev"))).click();
+        await driver.sleep(2000);
+        const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+        expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+    });
+
+    after(async () => {
+        await driver.quit()
+    });
+});
