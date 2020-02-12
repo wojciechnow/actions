afa77dd3ca626b8781d49dba2ece290fe54f735e
wojciechnowacki <wojciech.nowacki@softwarebrothers.co>
2020-02-12 12:33:39 +0100

    run flow according to user

--- test/randome.js
+++ test/randome.js
@@ -26,35 +26,21 @@ describe('my test', () => {
             .build();
     });
 
-    const x = process.env.pusher;
-    console.log(x)
-    if (x.includes('artur')) {
-        it('Just testing flow for selenium in SB company', async () => {
-            await driver.manage().window().maximize();
-            await driver.get('https://softwarebrothers.co/');
-            await driver.findElement(By.linkText("Services")).click();
-            await driver.sleep(2000);
-            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
-            await driver.sleep(2000);
-            await driver.findElement(By.className(("webdev"))).click();
-            await driver.sleep(2000);
-            const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-            expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-        });
-    } else {
-        it('Pusher testing', async () => {
-            await driver.manage().window().maximize();
-            await driver.get('https://softwarebrothers.co/');
-            await driver.findElement(By.linkText("Services")).click();
-            await driver.sleep(2000);
-            await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
-            await driver.sleep(2000);
-            await driver.findElement(By.className(("webdev"))).click();
-            await driver.sleep(2000);
-            const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-            expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-        });
-    }
+    const pusherEmail = process.env.pusher;
+    const name = pusherEmail.split('@')[0];
+    it('Just testing flow for selenium in SB company', async () => {
+        await driver.manage().window().maximize();
+        await driver.get(process.env.user);
+        await driver.findElement(By.linkText("Services")).click();
+        await driver.sleep(2000);
+        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+        await driver.sleep(2000);
+        await driver.findElement(By.className(("webdev"))).click();
+        await driver.sleep(2000);
+        const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+        expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+    });
+
 
     after(async () => {
         await driver.quit()
